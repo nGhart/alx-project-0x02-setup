6187cd065e0f5c9d965e6e24e4e9a978df331e11
oGhartey <ondghartey@gmail.com>
2025-11-23 10:35:02 +0000

    added assets

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({
+  textColor = "text-white",
+  bgColor,
+  children,
+  type = "button",
+}: {
+  children: React.ReactNode;
+  textColor?: string;
+  bgColor?: string;
+  type: "button" | "submit" | "reset" | undefined;
+}) => {
+  return (
+    <button
+      type={type}
+      style={{ padding: "5px 8px", borderRadius: "10%" }}
+      className={`p-2 text-2xl rounded-md ${textColor} ${bgColor}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Button from "../common/Button";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  const imagePath = "/images/Case Minimalistic.png";
+  return (
+    <div className="bg-primary text-white flex justify-center items-center gap-4 p-8 h-8 hover:bg-red-500 cursor-pointer border-amber-900 rounded-full">
+      <Image
+        src={imagePath}
+        alt="A minimalistic case design"
+        width={24}
+        height={24}
+        priority
+      />
+      <p>Overseas trip? Get the latest information on travel guides</p>
+      <Button bgColor="bg-secondary rounded-full" textColor="text-white">
+        More info
+      </Button>
+    </div>
+  );
 };
 
 export default Header;
